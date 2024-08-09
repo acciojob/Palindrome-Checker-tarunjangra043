516b1bef699fcf9c8e8d47c81e7cd641efe03734
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-08-09 08:20:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// complete the given function
-
 function palindrome(str){
+	    const normalizedStr = str
+        .toLowerCase();
+        .replace(/[^a-z0-9]/g, ''); 
 
+    const reversedStr = normalizedStr.split('').reverse().join('');
+    return normalizedStr === reversedStr;
 }
 module.exports = palindrome
